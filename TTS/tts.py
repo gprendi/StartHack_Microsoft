7206c004faef21af71f368ac541bc536d9ea0e68
gerald Prendi <gerald.prendi@usi.ch>
2021-03-20 10:33:54 +0100

    styles

--- TTS/tts.py
+++ TTS/tts.py
@@ -1,5 +1,16 @@
+import pyttsx3
 from gtts import gTTS
 import os
+
+
+def tts(text):
+    engine = pyttsx3.init()
+    engine.say('This is a text')
+    engine.runAndWait()
+    voices = engine.getProperty('voices')
+    
+    
+    engine.runAndWait()
 
 def text_to_speech(file_path):
 
@@ -10,5 +21,3 @@ def text_to_speech(file_path):
     tts = gTTS(text=text, lang='en')
 
     tts.save("test.mp3")
-
-text_to_speech("out_text.txt")
