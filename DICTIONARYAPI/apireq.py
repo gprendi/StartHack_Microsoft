970834e5650d4865369470e9c768e9659ec37e1f
USI <edo.salvioni@gmail.com>
2021-03-21 00:43:18 +0100

    ok

--- DICTIONARYAPI/apireq.py
+++ DICTIONARYAPI/apireq.py
@@ -3,22 +3,24 @@ import requests
 import json
 
 def request_definition(word):
-        #config = Config()
-        response = requests.get('https://www.dictionaryapi.com/api/v3/references/medical/json/%s?key=%s'%
-            (word,API_KEY))
-        text= response.json()
-        #loaded = text[-1]['shortdef']
-        #result =(word.title() + ': ')
-        #for i in loaded:
-        #    result += i + '\n'
-        #return result
+    #config = Config()
+    
+    response = requests.get('https://www.dictionaryapi.com/api/v3/references/medical/json/%s?key=%s'%
+        (word,API_KEY))
+    text= response.json()
+    #loaded = text[-1]['shortdef']
+    #result =(word.title() + ': ')
+    #for i in loaded:
+    #    result += i + '\n'
+    #return result
+    try:
         loaded= text[0]['def'][0]['sseq'][0][0][1]['dt'][0][1]
         return loaded
-
+    except:
+        return None
 
 
 
         
-
 
 
--- DICTIONARYAPI/config.py
+++
@@ -1,3 +0,0 @@
-class Config():
-    def  __init__(self):
-        self.API_KEY='2646fba5-255e-4987-847d-e26d98848b40'
