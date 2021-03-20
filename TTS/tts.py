b4418c583e7f3a3252c0adc291c0cf9be3082ad1
gerald Prendi <gerald.prendi@usi.ch>
2021-03-20 09:15:09 +0100

    Revert "TTS still experimental"
    This reverts commit 2b5dcfff19f8fa5b0cf5075d6fc1bb4738fc4040.

--- TTS/tts.py
+++ TTS/tts.py
@@ -1,17 +1,5 @@
-import pyttsx3
 from gtts import gTTS
 import os
-
-
-def tts(text):
-    engine = pyttsx3.init()
-    engine.say('This is a text')
-    engine.runAndWait()
-    voices = engine.getProperty('voices')
-    for v in range(len(voices)):
-        engine.setProperty('voice', voices[v].id)
-        engine.say('I am voice number,' + str(v))
-    engine.runAndWait()
 
 def text_to_speech(file_path):
 
@@ -23,5 +11,4 @@ def text_to_speech(file_path):
 
     tts.save("test.mp3")
 
-#text_to_speech("out_text.txt")
-tts('geri')
+text_to_speech("out_text.txt")
