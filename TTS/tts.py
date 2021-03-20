34db9a53fda497390dc1803c4d5f1197e6fd8512
gerald Prendi <gerald.prendi@usi.ch>
2021-03-20 09:07:01 +0100

    TTS still experimental

--- TTS/tts.py
+++ TTS/tts.py
@@ -1,5 +1,17 @@
+import pyttsx3
 from gtts import gTTS
 import os
+
+
+def tts(text):
+    engine = pyttsx3.init()
+    engine.say('This is a text')
+    engine.runAndWait()
+    voices = engine.getProperty('voices')
+    for v in range(len(voices)):
+        engine.setProperty('voice', voices[v].id)
+        engine.say('I am voice number,' + str(v))
+    engine.runAndWait()
 
 def text_to_speech(file_path):
 
@@ -11,4 +23,5 @@ def text_to_speech(file_path):
 
     tts.save("test.mp3")
 
-text_to_speech("out_text.txt")
+#text_to_speech("out_text.txt")
+tts('geri')
