60295e2d77f9cd79a701f07c54e64f13d31c97e2
gerald Prendi <gerald.prendi@usi.ch>
2021-03-20 01:24:59 +0100

    OCR uploaded and ready

---
+++ OCR/ocr.py
@@ -0,0 +1,66 @@
+
+
+from PIL import Image 
+import pytesseract 
+import sys 
+from pdf2image import convert_from_path 
+import os 
+
+
+def OCR_PDF(pdf_path):
+    # Path of the pdf 
+    PDF_file = pdf_path
+    
+    ''' 
+    Part #1 : Converting PDF to images 
+    '''
+    
+    # Store all the pages of the PDF in a variable 
+    pages = convert_from_path(PDF_file, 500) 
+    
+    # Counter to store images of each page of PDF to image 
+    image_counter = 1
+    
+    # Iterate through all the pages stored above 
+    for page in pages: 
+    
+        # PDF page n -> page_n.jpg 
+        filename = "page_"+str(image_counter)+".jpg"
+        
+        # Save the image of the page in system 
+        page.save(filename, 'JPEG') 
+    
+        # Increment the counter to update filename 
+        image_counter = image_counter + 1
+    
+    ''' 
+    Part #2 - Recognizing text from the images using OCR 
+    '''
+    # Variable to get count of total number of pages 
+    filelimit = image_counter-1
+    
+    # Creating a text file to write the output 
+    outfile = "out_text.txt"
+    
+    # Open in append mode
+    f = open(outfile, "a") 
+
+    # loop through the images
+    for i in range(1, filelimit + 1): 
+    
+        # Set filename to recognize text from 
+        filename = "page_"+str(i)+".jpg"
+            
+        # Recognize the text as string in image using pytesserct 
+        text = str(((pytesseract.image_to_string(Image.open(filename))))) 
+    
+        text = text.replace('-\n', '')     
+        
+        # Finally, write the processed text to the file. 
+        f.write(text) 
+
+        os.remove(filename)
+
+    # Close the file after writing all the text. 
+    f.close()
+
